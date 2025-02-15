69e753f9ccf5a87de9df61b646da79f0a4d35966
josei-wakamatsu <j.wakamatsu@e-showa.net>
2025-02-16 01:03:34 +0900

    フロントエンドのプログラムを更新

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Vite の設定
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ 追加
-  server: {
-    port: 5175,
-    strictPort: true,
-  },
+  root: './',  // ← ここが `./` になっているか確認
 });
